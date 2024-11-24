ae075d75a9037accbd0ed7c99e8ba3dc25cd6ef0
Axx <Sashik95@yandex.ru>
2024-11-24 21:33:48 +0500

    added class Book

---
+++ main.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self, id: int, title: str, author: str, year: int, status: str):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.year = year
+        self.status = status
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
