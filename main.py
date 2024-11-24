3ffaaa6cfcee744d770be60106c295c037a39d6a
Axx <Sashik95@yandex.ru>
2024-11-25 00:47:58 +0500

    Transferred command_handler from main.py in commands.py

--- main.py
+++ main.py
@@ -1,42 +1,5 @@
-from inspect import ismethod
-
 from library.library import Library
-from utils.helper import print_help
-from utils.commands import Command
-
-
-def command_handler(command_object: Command, library: Library) -> None:
-    """Обрабатывает команды"""
-
-    command_map = {
-        "add_book": library.add_book,
-        "remove_book": library.remove_book,
-        "find_book": library.find_book,
-        "change_status": library.change_status,
-        "print_books": lambda: print(library),
-        "help": print_help,
-        "save": library.save_to_file,
-        "load": library.load_from_file,
-    }
-
-    if command_object.command not in command_map:
-        print("Неизвестная команда. Используйте 'help' для справки")
-        return
-
-    if ismethod(command_map[command_object.command]):
-        expected_args = command_map[command_object.command].__code__.co_argcount - 1
-    else:
-        expected_args = command_map[command_object.command].__code__.co_argcount
-
-    if len(command_object.args) != expected_args:
-        print(f"Команда '{command_object.command}' "
-              f"ожидает {expected_args} аргументов, получено {len(command_object.args)}")
-        return
-
-    if command_object.args:
-        command_map[command_object.command](*command_object.args)
-    else:
-        command_map[command_object.command]()
+from utils.commands import Command, command_handler
 
 
 def main():
--- utils/commands.py
+++ utils/commands.py
@@ -1,4 +1,9 @@
 import re
+from inspect import ismethod
+
+from library.library import Library
+from utils.helper import print_help
+
 
 class Command:
     def __init__(self, command: str):
@@ -15,3 +20,37 @@ class Command:
     @property
     def args(self) -> list:
         return self._args
+
+
+def command_handler(command_object: Command, library: Library) -> None:
+    """Обрабатывает команды"""
+
+    command_map = {
+        "add_book": library.add_book,
+        "remove_book": library.remove_book,
+        "find_book": library.find_book,
+        "change_status": library.change_status,
+        "print_books": lambda: print(library),
+        "help": print_help,
+        "save": library.save_to_file,
+        "load": library.load_from_file,
+    }
+
+    if command_object.command not in command_map:
+        print("Неизвестная команда. Используйте 'help' для справки")
+        return
+
+    if ismethod(command_map[command_object.command]):
+        expected_args = command_map[command_object.command].__code__.co_argcount - 1
+    else:
+        expected_args = command_map[command_object.command].__code__.co_argcount
+
+    if len(command_object.args) != expected_args:
+        print(f"Команда '{command_object.command}' "
+              f"ожидает {expected_args} аргументов, получено {len(command_object.args)}")
+        return
+
+    if command_object.args:
+        command_map[command_object.command](*command_object.args)
+    else:
+        command_map[command_object.command]()
