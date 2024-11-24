2b85474a5c6778534544abfe6a97daab497a59f3
Axx <Sashik95@yandex.ru>
2024-11-25 00:38:05 +0500

    added all logic to main

--- main.py
+++ main.py
@@ -41,6 +41,14 @@ def command_handler(command_object: Command, library: Library) -> None:
 
 def main():
     library = Library()
+    print("Введите 'help' для получения списка команд")
+    while True:
+        command_object = Command(input())
+
+        if command_object.command.lower() == "exit":
+            break
+
+        command_handler(command_object, library)
 
 
 if __name__ == '__main__':
